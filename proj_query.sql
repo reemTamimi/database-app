aa7eac7f54c1f1558fc01b3e5b3fbcbf66864a11
jckwayne <120124641+jckwayne@users.noreply.github.com>
2023-02-18 17:54:34 -0500

    Update proj_query.sql
    Added relationship tables and included check constraints.

--- proj_query.sql
+++ proj_query.sql
@@ -1,8 +1,14 @@
 create database projdb;
 use projdb;
+
+############
+# ENTITIES #
+############
 create table contestant (
 	walletAddress varchar(42) not null,
-    rewardBalance double,
+	rewardBalance double,
+    check (regexp_like (walletAddress, '^(0x[A-F[:digit:]]{40})$')),
+    check (rewardBalance >= 0),
     unique(walletAddress),
     primary key (walletAddress));
 
@@ -11,10 +17,30 @@ create table contest (
     title varchar(100) not null,
     startDate date not null,
     endDate date not null,
-    status varchar(20) not null,
+    contestStatus varchar(20) not null,
     sponsorFee double not null,
     requirements varchar(1000) not null,
+    check (regexp_like (walletAddress, '^([0xA-F[:digit:]]{40})$')),
+    check (regexp_like (contestStatus, '^(created|opened|closed|past)$')),
+    check (sponsorFee >= 0),
+    check (datediff(endDate,startDate) >= 1),
     unique (walletAddress),
+    primary key (walletAddress));
+    
+create table sponsor (
+	walletAddress varchar(42) not null,
+    address varchar(200) not null,
+    companyName varchar(100) not null,
+    email varchar(100) not null,
+    check (regexp_like (walletAddress, '^([0xA-F[:digit:]]{40})$')),
+    check (regexp_like (email, '.+@.+')),
+    primary key (walletAddress));
+    
+create table judge (
+	walletAddress varchar(42) not null,
+    rewardBalance double,
+    check (regexp_like (walletAddress, '^([0xA-F[:digit:]]{40})$')),
+    check (rewardBalance >= 0),
     primary key (walletAddress));
     
 create table submission (
@@ -24,35 +50,44 @@ create table submission (
     foreign key (contestantWallet) references contestant (walletAddress),
     foreign key (contestWallet) references contest (walletAddress));
     
-create table sponsor (
-	walletAddress varchar(42) not null,
-    address varchar(200) not null,
-    companyName varchar(100) not null,
-    email varchar(100) not null,
-    primary key (walletAddress));
-    
-create table judge (
-	walletAddress varchar(42) not null,
-    rewardBalance double,
-    primary key (walletAddress));
-    
+
+#################
+# RELATIONSHIPS #
+#################
 create table review (
 	judgeWallet varchar(42) not null,
 	sponsorWallet varchar(42) not null,
     score int not null,
-    comment varchar(1000),
-    unique (judgeWallet, sponsorWallet),
+    reviewComment varchar(1000),
+    check (score >= 0 and score <= 10),
+    unique (sponsorWallet),
     foreign key (judgeWallet) references judge (walletAddress),
     foreign key (sponsorWallet) references sponsor (walletAddress),
-    primary key (judgeWallet, sponsorWallet));
+    primary key (sponsorWallet));
     
-create table grade (
+create table submissionGrade (
 	contestantWallet varchar(42) not null,
     contestWallet varchar(42) not null,
     judgeWallet varchar(42) not null,
     grade int not null,
+    check (grade >= 0 and grade <= 100),
 	primary key (contestantWallet, contestWallet, judgeWallet),
     foreign key (contestantWallet) references contestant (walletAddress),
     foreign key (contestWallet) references contest (walletAddress),
     foreign key (judgeWallet) references judge (walletAddress));
-	
+    
+create table contestJudge (
+	contestWallet varchar(42) not null,
+	judgeWallet varchar(42) not null,
+	foreign key (contestWallet) references contest (walletAddress),
+	foreign key (judgeWallet) references judge (walletAddress),
+    unique (contestWallet, judgeWallet),
+    primary key (contestWallet, judgeWallet));
+    
+create table contestSponsor (
+	contestWallet varchar(42) not null,
+    sponsorWallet varchar(42) not null,
+    unique (contesteWallet),
+	foreign key (contestWallet) references contest (walletAddress),
+	foreign key (sponsorWallet) references sponsor (walletAddress),
+    primary key (contestWallet));
