4387bfb94737618f89f0bda6ffadecf1fde33f5d
jckwayne <120124641+jckwayne@users.noreply.github.com>
2023-02-23 17:57:54 -0500

    this one! userDAO

--- database/src/userDAO.java
+++ database/src/userDAO.java
@@ -45,7 +45,7 @@ public class userDAO
             } catch (ClassNotFoundException e) {
                 throw new SQLException(e);
             }
-            connect = (Connection) DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/testdb?allowPublicKeyRetrieval=true&useSSL=false&user=john&password=pass1234");
+            connect = (Connection) DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/projdb?allowPublicKeyRetrieval=true&useSSL=false&user=john&password=pass1234");
             System.out.println(connect);
         }
     }
