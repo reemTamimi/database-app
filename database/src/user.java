e226e271780287738aee34e120a51a9a95e03759
jckwayne <120124641+jckwayne@users.noreply.github.com>
2023-03-08 12:53:02 -0500

    Update user.java

--- database/src/user.java
+++ database/src/user.java
@@ -1,18 +1,5 @@
 public class user 
-{
-		/*protected String password;
-	 	protected String email;
-	    protected String firstName;
-	    protected String lastName;
-	    protected String adress_street_num;
-	    protected String adress_street;
-	    protected String adress_city;
-	    protected String adress_state;
-	    protected String adress_zip_code;
-	    protected String birthday;
-	    protected int cash_bal;
-	    protected int PPS_bal;*/
-	    
+{	    
 	     protected String walletAddress;
 	     protected String pass;
 	     protected String userRole;
@@ -24,14 +11,11 @@ public class user
 	    public user(String walletAddress) 
 	    {
 	    	this.walletAddress = walletAddress;
-	        //this.email = email;
 	    }
 	    
 	    
 	    public user(String walletAddress,String pass, String userRole) 
 	    {
-	    	//this(firstName,lastName,password,birthday, adress_street_num,  adress_street,  adress_city,  adress_state,  adress_zip_code,cash_bal,PPS_bal);
-	    	//this.email = email;
 	    	this(pass,userRole);
 	    	this.walletAddress = walletAddress;
 	    }
