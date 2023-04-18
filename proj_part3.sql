2b1e4194ba4117974ea164961599c35b6699f5a4
jckwayne <120124641+jckwayne@users.noreply.github.com>
2023-04-18 11:13:25 -0400

    #5 common contests

--- proj_part3.sql
+++ proj_part3.sql
@@ -44,6 +44,11 @@ select walletAddress from contestant a
 join (select distinct rewardBalance from contestant order by rewardBalance desc limit 1) b
 on a.rewardBalance = b.rewardBalance;
 
+# 5. common contests
+select contestWallet from submission
+where contestantWallet in ('0x000000000000000000000000000000000000001A','0x000000000000000000000000000000000000007A')
+group by contestWallet having count(contestantWallet)=2;
+
 # 6. sleepy contestants
 select u.walletAddress from users u where userRole = "contestant" and u.walletAddress not in 
 (select s.contestantWallet from submission s);
